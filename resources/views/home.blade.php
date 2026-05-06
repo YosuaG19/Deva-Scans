0189ad21a10e18d526bae40af13b7f646b826865
YosuaG19 <yosuawg19@gmail.com>
2026-05-06 15:44:01 +0700

    Add: project template

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Deva Scans</title>
+        @vite('resources/css/app.css')
+    </head>
+
+    <body class="bg-primary">
+        <div class="flex justify-center items-center h-[100vh]">
+            <h1 class="text-[#454545]">Whihihihihih</h1>
+        </div>
+    </body>
+</html>
