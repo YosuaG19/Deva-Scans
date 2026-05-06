4b5faf289acf0ef58af969ec425819a8a78d8bb5
YosuaG19 <yosuawg19@gmail.com>
2026-05-06 15:50:16 +0700

    Add: project template

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
 
     <body class="bg-primary">
         <div class="flex justify-center items-center h-[100vh]">
-            <h1 class="text-[#454545]">Whihihihihih</h1>
+            <h1 class="text-[#454545]">Whihihihihih hahahahaha</h1>
         </div>
     </body>
 </html>
