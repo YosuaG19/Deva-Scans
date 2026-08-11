aa0e32087c37ac2cc680ed9854708ab33c78d06c
Yosua W G <119386767+YosuaG19@users.noreply.github.com>
2026-08-11 10:25:36 +0700

    Refactor homeView to update comics retrieval logic

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,12 +9,14 @@ class HomeController extends Controller
 {
     public function homeView(){
         $comics_banner = Comics::take(20)->get();
-        $comics_upt = Comics::orderBy('created_at', 'desc')->take(5)->get();
         $comics_rec = Comics::orderBy('rating_avg', 'desc')->take(5)->get();
 
 
-        $updates = Comics::all()->groupBy('upt_day');        
+        $updates = Comics::orderBy('created_at', 'desc')
+            ->get()
+            ->groupBy('upt_day')
+            ->map(fn ($comics) => $comics->take(6));          
 
-        return view('home', compact('comics_banner', 'comics_upt', 'comics_rec', 'updates'));
+        return view('home', compact('comics_banner', 'comics_rec', 'updates'));
     }
 }
